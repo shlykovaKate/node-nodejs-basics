2e6cc5cfdd3e96472e39f1916760c3389df9283c
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-04 23:59:38 +0300

    feat: implement 'copy' function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,25 @@
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { cp } from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const sourcePath = resolve(__filename, '../files');
+    const destinationPath = resolve(__filename, '../files_copy');
+    const options = {
+        errorOnExist: true,
+        force: false,
+        recursive: true
+    }
+
+    try {
+        await cp(sourcePath, destinationPath, options);
+    } catch (error) {
+        if (error.code === 'ERR_FS_CP_EEXIST' || error.code ==='ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw new Error(error.message);
+    }
 };
+
+copy();
