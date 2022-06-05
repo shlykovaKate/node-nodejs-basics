83d4275673005d2549cb8cb6f4a17d4ab21a7330
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 21:22:00 +0300

    feat: implement 'write' function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,32 @@
+import { appendFile } from 'fs/promises';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { ReadableStream, WritableStream } from 'node:stream/web';
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const path = resolve(__filename, '../files/fileToWrite.txt');
+
+    const readableStream = new ReadableStream({
+        pull(controller) {
+            process.stdin.once('readable', () => {
+                const chunk = process.stdin.read();
+                if (chunk) {
+                    controller.enqueue(chunk);
+                }
+            });
+        }
+    });
+
+    const writableStream = new WritableStream({
+       async write(chunk) {
+            if (chunk) {
+               await appendFile(path, chunk.toString('utf-8'));
+            }
+        }
+    });
+
+    readableStream.pipeTo(writableStream);
 };
+
+write();
