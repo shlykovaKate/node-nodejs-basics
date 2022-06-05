09a9a5c34af136fc4880f06ca167243a0c460e5c
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 21:21:12 +0300

    feat: implement 'read' function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,29 @@
+
+import { ReadableStream, WritableStream } from 'node:stream/web';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const path = resolve(__filename, '../files/fileToRead.txt');
+
+    const readableStream = new ReadableStream({
+        async start(controller) {
+            const file = await readFile(path);
+            controller.enqueue(file);
+        }
+    });
+
+    const writableStream = new WritableStream({
+        write(chunk) {
+            if (chunk) {
+                process.stdout.write(`${chunk}\n\n`);
+            }
+        }
+    });
+
+    readableStream.pipeTo(writableStream);
 };
+
+read();
