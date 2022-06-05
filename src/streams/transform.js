32e1635b5d4553fe39d24763e71b0cda4c9964ca
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 21:22:58 +0300

    feat: implement 'transform' function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,34 @@
+import { ReadableStream, TransformStream, WritableStream } from 'node:stream/web';
+
 export const transform = async () => {
-    // Write your code here 
+    const readableStream = new ReadableStream({
+        pull(controller) {
+            process.stdin.once('readable', () => {
+                const chunk = process.stdin.read();
+                if (chunk) {
+                    controller.enqueue(chunk);
+                }
+            });
+        }
+    });
+
+    const transformStream = new TransformStream({
+        transform(chunk, controller) {
+            if (chunk) {
+                controller.enqueue(chunk.toString().split("").reverse().join(""));
+            }
+        }
+    });
+
+    const writableStream = new WritableStream({
+        write(chunk) {
+            if (chunk) {
+                process.stdout.write(`${chunk}\n\n`);
+            }
+        }
+    });
+
+    readableStream.pipeThrough(transformStream).pipeTo(writableStream);
 };
+
+transform();
