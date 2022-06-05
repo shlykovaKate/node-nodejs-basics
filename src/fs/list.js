3750fa1a63b188cd5642035f40c12b9d7a1f45c7
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 10:19:12 +0300

    feat: implement 'list' function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+
 export const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const path = resolve(__filename, '../files');
+
+    try {
+        const files = await readdir(path);
+        console.log('FILES', files);
+    } catch (error) {
+        if (error.code ==='ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw new Error(error.message);
+    }
 };
+
+list();
