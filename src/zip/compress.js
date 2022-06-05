c0530ffa52dd76abbb7c017f114d5f1c60f42a88
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 21:24:18 +0300

    feat: implement 'compress' function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 export const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const zipPath = resolve(__filename, '../files/fileToCompress.txt');
+    const gzPath = resolve(__filename, '../files/archive.gz');
+    const zip = createGzip();
+    const readableStream = createReadStream(zipPath);
+    const writableStream = createWriteStream(gzPath);
+
+    readableStream.pipe(zip).pipe(writableStream);
 };
+
+compress();
