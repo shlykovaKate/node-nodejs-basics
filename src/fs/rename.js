954894e654b19402fffba83bdbdb5ce35a7acdbb
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-05 09:55:50 +0300

    feat: implement 'rename' function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,29 @@
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import * as fs from 'fs/promises';
+
 export const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const oldPath = resolve(__filename, '../files/wrongFilename.txt');
+    const newPath = resolve(__filename, '../files/properFilename.md');
+
+    try {
+        const file = await fs.readFile(newPath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code ==='ENOENT') {
+            try {
+                await fs.rename(oldPath, newPath);
+            } catch (error) {
+                if (error.code ==='ENOENT') {
+                    throw new Error('FS operation failed');
+                }
+                throw new Error(error.message);
+            }
+        } else {
+            throw new Error('FS operation failed');
+        }
+    }
 };
+
+rename();
